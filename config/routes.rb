824b3ffc0e502b2baa86bcf208871380e3ef9549
Alex Diwa <atdiwa@gmail.com>
2019-04-28 20:41:44 +1000

    added authentication and fixed user migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  devise_for :users
+  root to: "conversations#index"
   resources :conversations, only: [:index, :create] do
     resources :messages, only: [:index, :create]
   end
